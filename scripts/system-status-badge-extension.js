3bfb98a73e9e5035211f5f6d91466ff3331cb274
Guillermo Gomez Savino <ggomezsa@redhat.com>
2021-03-02 12:56:12 +0100

    https scheme needed

--- scripts/system-status-badge-extension.js
+++ scripts/system-status-badge-extension.js
@@ -7,7 +7,7 @@ angular
     function(extensionRegistry) {
 
       //http://paas.stage.psi.redhat.com
-      var system_status_elem = $('<a href="http://paas.stage.psi.redhat.com/"' +
+      var system_status_elem = $('<a href="https://paas.stage.psi.redhat.com/"' +
       'target="_blank" class="nav-item-iconic system-status"><span title="' +
       'System Status" class="fa status-icon pficon-warning-triangle-o">' +
       '</span>System Status</a>');
